fbd5f20e38dbeff9c064efa08ae42b14b3465080
DarioIEL <104058513+DarioIEL@users.noreply.github.com>
2022-06-23 12:57:38 +0200

    agg

--- workspace-java-backend/Prj14_Contrabbando/src/main/java/controller/Opere.java
+++ workspace-java-backend/Prj14_Contrabbando/src/main/java/controller/Opere.java
@@ -5,7 +5,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Oggetto;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import database.Oggetti;
 
 /**
  * Servlet implementation class Libri
@@ -26,8 +32,18 @@ public class Opere extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("titolo", "Contrabbando libri");
+		
+		request.getRequestDispatcher("header.jsp").include(request, response);
+		request.getRequestDispatcher("menu.jsp").include(request, response);
+		Oggetti.init();
+		List<Oggetto> oggetti = Oggetti.deposito
+				.stream()
+				.filter(o -> o.getTipo().equalsIgnoreCase("opera"))
+				.collect(Collectors.toList());
+		request.setAttribute("oggettiContrabbando", oggetti);
+		request.getRequestDispatcher("tabella.jsp").include(request, response);
+		request.getRequestDispatcher("footer.jsp").include(request, response);
 	}
 
 	/**
